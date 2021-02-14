0df532f44fed7003223361f9df3ad30c536bf9c2
Ricard Bejarano <ricard@bejarano.network>
2021-02-14 03:10:37 +0100

    Revert "removed derivation of acme_certificate attributes (preserves attribute sensitivity degree, in exchange for exposing the internal acme_certificate objects, which at the end of the day is derived from user-provided values, which is better than exposing sensitive values during plan)"
    This reverts commit 3c5e65442149006584e222bdc257659eeba3e228.

--- outputs.tf
+++ outputs.tf
@@ -13,5 +13,13 @@ output "registration" {
 
 output "certificates" {
   description = "The certificates and their contents."
-  value       = acme_certificate.certificates
+  value = [for certificate in acme_certificate.certificates : {
+    id                 = certificate.id
+    certificate_url    = certificate.certificate_url
+    certificate_domain = certificate.certificate_domain
+    private_key_pem    = certificate.private_key_pem
+    certificate_pem    = certificate.certificate_pem
+    issuer_pem         = certificate.issuer_pem
+    certificate_p12    = certificate.certificate_p12
+  }]
 }
